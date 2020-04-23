c38b73754a8fd60630c11d634a8a30ee86c1a9f5
badari07 <badari0793@gmail.com>
2020-04-24 00:31:24 +0530

    feat: chrome dev

--- public/js/mark-and-measure.js
+++ public/js/mark-and-measure.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
+  performance.mark("start");
   fetch("/api")
-    .then(result => result.json())
-    .then(json => {
-      json.images.forEach(image => console.log(image.name));
+    .then((result) => result.json())
+    .then((json) => {
+      json.images.forEach((image) => console.log(image.name));
+      performance.mark("end");
+      performance.measure("Our Measurement", "start", "end");
+      const time = performance.getEntriesByType("measure");
+      console.log(time);
     });
 }
